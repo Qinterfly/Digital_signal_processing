ec330eeb154f2fb2abb6446992f2736c74c63c29
Qinterfly <qinterfly@gmail.com>
2018-11-09 18:33:49 +0700

    v.3.9. - Добавлено масштабирование по физическому множителю при сохранении сигналов; - Добавлен режим отображения угловых поверхностей рассеяния

--- OutputSaveTxt.m
+++ OutputSaveTxt.m
@@ -7,10 +7,12 @@ if ~isdir(Path) %Создание директории для данного сигнала
 end
 fileID = fopen(strcat(Path,'/',FileName,'.txt'),'w'); %Открытие файла для записи
 TechnicalData{end} = num2str(size(OutputSignal,1)); %Запись в технические сведения реальной длины выходного сигнала
+PhysFactor = str2num(TechnicalData{7}); % Физический параметр сигнала
 for i = 1:length(TechnicalData)
     fprintf(fileID,'%s \r\n',TechnicalData{i}); %Запись технических сведений
 end
 formatSpec = '%f \r\n'; %Формат записи значений
+OutputSignal = OutputSignal / PhysFactor; % Масштабирование по физическому параметру сигнала 
 if size(OutputSignal,2) > 1
     dlmwrite(strcat(Path,'/',FileName,'.txt'),OutputSignal,'-append','delimiter','\t','newline','pc');
 else
